25030c1338b8b6b2c37a97ed67b125d93b637fe3
sonshn <sonshn202064@gmail.com>
2022-06-13 13:47:50 +0900

    Creating navigation bar & Routing malfunction

--- movie_app_2022/src/index.js
+++ movie_app_2022/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+const rootNode = document.getElementById('root');
+
+ReactDOM.createRoot(rootNode).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
